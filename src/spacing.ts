484b6ab15066e66720149df99fa038e437dd7f30
Yony Calsin <helloyonycalsin@gmail.com>
2021-08-01 15:57:38 -0500

    feat: 🎸 update the spacing length

--- src/spacing.ts
+++ src/spacing.ts
@@ -1,8 +1,10 @@
 const spacingSize = 8
 
+const spacingLength = 30
+
 export const getSpacing = (count = 50) =>
   Array.from({ length: count + 1 }).map((_, index) => `${index * spacingSize}px`)
 
-const spacing = getSpacing(spacingSize)
+const spacing = getSpacing(spacingLength)
 
 export default spacing
