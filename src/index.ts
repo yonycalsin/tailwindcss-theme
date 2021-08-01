946d41abeff25c8dbb70fafb780cd81b41267803
Yony Calsin <helloyonycalsin@gmail.com>
2021-08-01 15:48:54 -0500

    feat: 🎸 updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop')
-  }
-  return a + b
+import colors from './colors'
+import spacing from './spacing'
+
+export default {
+  darkMode: 'class',
+  theme: {
+    colors,
+    spacing: {
+      ...spacing,
+      0.5: '4px',
+      1.5: '12px',
+    },
+  },
 }
