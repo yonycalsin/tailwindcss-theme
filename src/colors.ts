54d4b7ea64a317fba295b4707e1093da8193891b
Yony Calsin <helloyonycalsin@gmail.com>
2021-08-01 15:48:18 -0500

    feat: 🎸 add colors

---
+++ src/colors.ts
@@ -0,0 +1,95 @@
+export const commonColors = {
+  black: '#000000',
+  inherit: 'inherit',
+  transparent: 'transparent',
+  current: 'currentColor',
+  white: '#ffffff',
+}
+
+export const primary = {
+  50: '#D7FCF5',
+  100: '#97F7E2',
+  200: '#0EF2CE',
+  300: '#00E8B8',
+  400: '#00DCA8',
+  500: '#00D198',
+  DEFAULT: '#00C189',
+  700: '#00AF77',
+  800: '#009E68',
+  900: '#007F48',
+}
+
+export const secondary = {
+  50: '#FEF4E3',
+  100: '#FDE2B9',
+  200: '#FDCF8D',
+  300: '#FCBB62',
+  400: '#FBAD46',
+  500: '#FAA036',
+  DEFAULT: '#F59433',
+  700: '#EE852F',
+  800: '#E7772C',
+  900: '#DC6128',
+}
+
+export const gray = {
+  50: '#F3F9FF',
+  100: '#ECF1F7',
+  200: '#E2E7ED',
+  300: '#D1D7DD',
+  400: '#ADB3B8',
+  500: '#8D9298',
+  DEFAULT: '#656A70',
+  700: '#52575C',
+  800: '#34393D',
+  900: '#14191D',
+}
+
+export const error = {
+  50: '#FFE9EF',
+  100: '#FFC9D5',
+  200: '#F8919D',
+  300: '#F26276',
+  400: '#FF2D54',
+  500: '#FF0038',
+  DEFAULT: '#F70038',
+  700: '#E50031',
+  800: '#D80029',
+  900: '#C9001D',
+}
+
+export const success = {
+  50: '#E8F5E9',
+  100: '#C8E6C9',
+  200: '#A5D6A7',
+  300: '#81C784',
+  400: '#66BB6B',
+  500: '#4CAF51',
+  DEFAULT: '#43A048',
+  700: '#388E3D',
+  800: '#2E7D33',
+  900: '#1B5E21',
+}
+
+export const warning = {
+  50: '#FFFEE7',
+  100: '#FDFBC2',
+  200: '#FBF898',
+  300: '#F9F46B',
+  400: '#F6EF45',
+  500: '#FFF627',
+  DEFAULT: '#FFE21D',
+  700: '#FFC90C',
+  800: '#FFB000',
+  900: '#FF8500',
+}
+
+export default {
+  ...commonColors,
+  primary,
+  secondary,
+  gray,
+  error,
+  success,
+  warning,
+}
